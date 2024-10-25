e59947cfe0c24d355f2ce056694a3216b2d136fa
hyunsu.ryu <abba010327@gmail.com>
2024-10-26 01:50:03 +0900

    first commit

---
+++ test.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+#include<malloc.h>
+#include<stdbool.h>
+#include<stdint.h>
+#include<time.h>
+
+int main(int ArgsNum, char* Args[])
+{
+    printf("hello world\n");
+
+    return 0;
+}
